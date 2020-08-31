e3689e81c457d731efe7419c1fc4b719461faae1
HikaruEgashira <shighten@gmail.com>
2020-08-31 19:56:34 +0900

    ac

--- atcoder.jp/contests/abc177/src/bin/c.rs
+++ atcoder.jp/contests/abc177/src/bin/c.rs
@@ -5,7 +5,7 @@
 use proconio::input;
 
 #[proconio::fastout]
-fn main() {
+fn my_ans_is_tle() {
     input! {
         n: usize,
         a: [i64; n],
@@ -19,6 +19,31 @@ fn main() {
         .map(|na| a[na] * (na + 1..n).map(|nb| a[nb]).sum::<i64>())
         .sum::<i64>()
         % mod_value;
+
+    println!("{}", sum);
+}
+
+#[proconio::fastout]
+fn main() {
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+
+    let (n, a): (usize, Vec<i64>) = (n, a);
+    let mod_value = 1000000007;
+
+    // 累積和を求める
+    // b[i + 1] = b[i] + a[i]
+    let b = a.iter().fold(vec![], |mut bi: Vec<i64>, ai| {
+        bi.push(bi.last().unwrap_or(&0) + ai);
+        bi
+    });
+
+    let sum = (0..n).fold(0, |acc, ni| {
+        let sum = (b[n - 1] - b[ni]) % mod_value;
+        (acc + a[ni] * sum) % mod_value
+    });
 
     println!("{}", sum);
 }
--- atcoder.jp/contests/abc177/src/bin/d.rs
+++ atcoder.jp/contests/abc177/src/bin/d.rs
@@ -4,15 +4,90 @@
 **/
 use proconio::input;
 
-// fn push_vec(group: Vec<Vec<usize>>, a: usize, b: usize) -> Vec<Vec<usize>> {
-//     let mut out: Vec<Vec<usize>> = group.clone();
-//     out[a].push(b);
-//     out[b].push(a);
-//     out
-// }
+#[derive(Debug, Clone)]
+enum UF {
+    Root(usize),
+    Child(usize),
+}
+
+#[derive(Debug)]
+struct UnionFind {
+    table: Vec<UF>,
+}
+impl UnionFind {
+    // 初期化
+    fn new(n: usize) -> UnionFind {
+        UnionFind {
+            table: vec![UF::Root(1); n],
+        }
+    }
+    // 親
+    fn root(&mut self, x: usize) -> usize {
+        match self.table[x] {
+            UF::Root(_) => x,
+            UF::Child(parent) => {
+                let root = self.root(parent);
+                self.table[x] = UF::Child(root);
+                root
+            }
+        }
+    }
+    // 同じグループか判定
+    #[allow(dead_code)]
+    fn is_same(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+    // Root(size) ← sizeの値を出力
+    fn size(&mut self, x: usize) -> usize {
+        let r = self.root(x);
+        match self.table[r] {
+            UF::Root(size) => size,
+            UF::Child(_) => 0,
+        }
+    }
+    // 併合
+    fn merge(&mut self, x: usize, y: usize) {
+        let root_x = self.root(x);
+        let root_y = self.root(y);
+        if root_x != root_y {
+            let size_x = self.size(root_x);
+            let size_y = self.size(root_y);
+            let (i, j) = if size_x > size_y {
+                (root_x, root_y)
+            } else {
+                (root_y, root_x)
+            };
+            self.table[i] = UF::Root(size_x + size_y);
+            self.table[j] = UF::Child(i);
+        }
+    }
+}
 
 #[proconio::fastout]
 fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        a: [(usize, usize); m],
+    }
+
+    let a: Vec<(usize, usize)> = a;
+
+    // UnionFind木の作成
+    let mut uf = UnionFind::new(n);
+    for mi in 0..m {
+        let (x, y) = a[mi];
+        uf.merge(x - 1, y - 1);
+    }
+
+    // 木の大きさの最大値を求める
+    let ans = (0..n).map(|i| uf.size(i)).max().unwrap();
+
+    println!("{}", ans);
+}
+
+#[allow(dead_code)]
+fn my_ans_wa() {
     input! {
         n: usize,
         m: usize,
