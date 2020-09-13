e370b5f659ab1ae6b73f06f17d8f3235bd04e9e8
HikaruEgashira <shighten@gmail.com>
2020-09-13 07:50:12 +0000

    init

--- playground/tests/map.rs
+++ playground/tests/map.rs
@@ -48,19 +48,3 @@ pub fn map_example() {
 
     assert_eq!(v, [2, 1, 0]);
 }
-
-#[test]
-fn collect_example() {
-    let a = [1, 2, 3];
-    let doubled = a.iter().map(|&x| x * 2).collect::<Vec<i32>>();
-
-    assert_eq!(vec![2, 4, 6], doubled);
-
-    let one_to_one_hundred = (1..101).collect::<Vec<i32>>();
-    let one_to_one_hundred_2 = one_to_one_hundred
-        .iter()
-        .map(|&value| value)
-        .collect::<Vec<i32>>();
-
-    assert_eq!(one_to_one_hundred, one_to_one_hundred_2);
-}
